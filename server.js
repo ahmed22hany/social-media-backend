f9bb3fd4263f7da788e3cd6a0e5a5e450c2dca4f
ahmed22hany <ahaany5673@gmail.com>
2024-10-04 16:52:31 +0300

    dotenv-install

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+mongoose
+  .connect("", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is Connected"))
+  .catch((err) => console.log(err, "There Are an error with connection"));
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running now on ${PORT} port`));
